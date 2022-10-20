49ff1d9afd528c558df423725c54dd4a8ddbc06b
radenmasabdul <abdul.tamsis@gmail.com>
2022-10-20 17:04:46 +0700

    update function cardpost, postlogin, cardprofile, register, index, reducer

--- src/components/CardsProfiles.jsx
+++ src/components/CardsProfiles.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 import Friendslist from './Friendslist';
-
+import { useDispatch, useSelector } from "react-redux";
+import { handleUser } from "utils/redux/reducers/reducer";
 import {BsEnvelope} from "react-icons/bs";
 import {FaUserFriends} from "react-icons/fa";
 import {MdGroups} from "react-icons/md";
@@ -9,6 +10,8 @@ import {AiOutlineLogout} from "react-icons/ai"
 import { apiRequest } from 'utils/apiRequest';
 
 function CardsProfiles() {
+  const Users = useSelector((state) => state.data.Users);
+  const dispatch = useDispatch();
   const [id, setId] = useState("");
   const [name, setName] = useState("");
   const [Password, setPassword] = useState("");
@@ -16,7 +19,16 @@ function CardsProfiles() {
   const [hp, setHp] = useState("");
   const [bio, setBio] = useState("");
   const [images, setImages] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(true);
 
+  useEffect(() => {
+    if (id && name && Password && Email && hp && bio && images){
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [id, name,Password,Email,hp,bio,images]);
   useEffect(() => {
     fetchDataProfiles();
   },[]);
@@ -27,21 +39,20 @@ function CardsProfiles() {
     apiRequest(`http://52.77.235.98/users/${id}`, "GET")
     .then ((res) => {
         localStorage.setItem('dataUser', JSON.stringify(res));
-        setId(res.data.id);
-        setName(res.data.name);
-        setPassword(res.data.Password);
-        setEmail(res.data.Email);
-        setHp(res.data.hp);
-        setBio(res.data.bio);
-        setImages(res.data.Images);
+        dispatch(handleUser(res.data));
       }
     )
     .catch ((err) => {
       const { message } = err.response.data;
       alert(message)
     })
-    .finally();
+    .finally(() =>setLoading(false)) ;
   } 
+
+  function clearLocalStorage () {
+    localStorage.clear();
+    window.location.reload();
+  }
 
   return (
     <>
@@ -49,16 +60,15 @@ function CardsProfiles() {
     <div className="w-full bg-bg-color rounder-lg border-gray-200 dark:bg-gray-800 dark:border-gray-700">
           <div className="grid justify-items-center pb-10 pt-10">
             <img className="mb-3 w-24 h-24 rounded-full shadow-lg" src="https://cdn.iconscout.com/icon/free/png-64/pokemon-pokeball-pikachu-thunder-electric-shock-32217.png" alt=""/>
-            <h5 className="mb-1 text-xl font-medium font-pt-sans text-bg-dark dark:text-bg-color">{name }</h5>
+            <h5 className="mb-1 text-xl font-medium font-pt-sans text-bg-dark dark:text-bg-color">{Users?.name}</h5>
             <span className="font-pt-sans text-sm text-bg-dark dark:text-bg-color">Lihat profile anda</span>
             <div className="grid grid-cols-2 gap-2 border-t-2 border-bg-dark ">
               <div className="my-4 px-8 w-20 text-bg-color2"><BsEnvelope/></div>
               <div className="my-4 px-8 w-20 text-bg-color2"><FaUserFriends/></div>
-              
             </div>
             <div className="grid grid-cols-2 gap-2 border-b-2 border-bg-dark ">
-            <div className="my-4 px-8 w-20 text-bg-color2"><MdGroups/></div>
-              <div className="my-4 px-8 w-20 text-bg-color2"><AiOutlineLogout /></div>
+              <div className="my-4 px-8 w-20 text-bg-color2"><MdGroups/></div>
+              <div className="my-4 px-8 w-20 text-bg-color2"><button onClick={clearLocalStorage} ><AiOutlineLogout/></button></div>
             </div>
             <p className="font-pt-sans text-md font-medium text-bg-dark dark:text-bg-color">Daftar Teman</p>
             <div className="grid-cols-3 grid mr-3">
@@ -68,7 +78,6 @@ function CardsProfiles() {
               <div className="ml-3 mt-2"><Friendslist/></div>
               <div className="ml-3 mt-2"><Friendslist/></div>
               <div className="ml-3 mt-2"><Friendslist/></div>
-
             </div>
           </div>
       </div>
